495c51b99363b623adca8b372d114b7931e2db52
rkstrdee <rkstrdee@gmail.com>
2019-05-30 17:07:56 +0530

    Added model for users and some api

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
+const connectDB =require('./config/db');
 
+//Connect Database
+connectDB();
 
 app.get('/',(req,res)=>res.send('API Running'));
 
-
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/auth',require('./routes/api/auth'));
 
 const PORT = process.env.PORT || 5000;
 

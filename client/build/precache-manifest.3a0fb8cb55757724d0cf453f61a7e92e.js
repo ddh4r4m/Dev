e5d394223abd68a2ed81dd8189d7f2db87d0886a
rkstrdee <rkstrdee@gmail.com>
2019-07-15 19:08:35 +0530

    build

---
+++ client/build/precache-manifest.3a0fb8cb55757724d0cf453f61a7e92e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e6729379bdc141b731845aaff4fc7c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9df5b2fcc794eedcf364",
+    "url": "/static/css/main.6b004c1e.chunk.css"
+  },
+  {
+    "revision": "8b27dfb36f9a5aa9e3d1",
+    "url": "/static/js/2.03187b8a.chunk.js"
+  },
+  {
+    "revision": "9df5b2fcc794eedcf364",
+    "url": "/static/js/main.0073bae8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7d23a296a277932038fca8f6774d4d3e",
+    "url": "/static/media/Componentformbg.7d23a296.png"
+  },
+  {
+    "revision": "cb951145bc4ae9d8288a8d9042cfcd9d",
+    "url": "/static/media/Ellipsecircle.cb951145.svg"
+  },
+  {
+    "revision": "14b5230f264114e763213a5115d995bf",
+    "url": "/static/media/Polygontriangle.14b5230f.svg"
+  },
+  {
+    "revision": "e23454391d57f2e063b7c7c4a52d1c52",
+    "url": "/static/media/Rectanglesquare.e2345439.svg"
+  },
+  {
+    "revision": "000bb44ed097204f22f27480beca5977",
+    "url": "/static/media/Vectorbackground.000bb44e.svg"
+  },
+  {
+    "revision": "0c194e0388774f957906aa97d5d0be50",
+    "url": "/static/media/spinner2.0c194e03.gif"
+  },
+  {
+    "revision": "20b3d3b45e46733fe79010f007f27ea9",
+    "url": "/static/media/spinners.20b3d3b4.gif"
+  }
+]);
